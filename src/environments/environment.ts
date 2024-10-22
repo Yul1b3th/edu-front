9446f47f474b6814025ec748d54f3de0a3cd3f68
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-10-22 22:05:21 +0200

    Changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://hackatonitacademy-4eb658690555.herokuapp.com/api/'
+  baseUrl: 'https://hackatonitacademy-4eb658690555.herokuapp.com/api/districts',
 };
