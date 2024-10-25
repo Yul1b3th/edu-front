a95901ea3ea1b980162e81e7aeebda70823df7f9
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-10-25 11:41:16 +0200

    Force untrack environments folder

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  mapboxToken:'pk.eyJ1IjoiYWRwdGNvZGUiLCJhIjoiY20yajNyM2wxMDFoaDJqc2I4dG5keXAzaCJ9.qIRLrPbj_pGnE0QzjbwkUw',
-  production: false,
-  baseUrl: '/api'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: 'https://hackatonitacademy-4eb658690555.herokuapp.com/api/districts',
-};
